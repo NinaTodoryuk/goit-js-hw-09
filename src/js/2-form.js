48bde9edf3a22e6462e8e9c1e311f0ab4089158f
NinaTodoryuk <nina.todoryuk@gmail.com>
2024-05-05 19:42:59 -1200

    ver1

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+const loadFormDataFromLocalStorage = () => {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+};
+
+loadFormDataFromLocalStorage();
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  formData[name] = value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+const isFormValid = () => {
+  return formData.email.trim() !== '' && formData.message.trim() !== '';
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!isFormValid()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
